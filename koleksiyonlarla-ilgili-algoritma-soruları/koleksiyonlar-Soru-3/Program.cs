e0ab6f7e21442a48241143f86b9b3fb4d30993fb
Your Name <you@example.com>
2022-02-05 23:56:20 +0300

    soru 3

---
+++ koleksiyonlarla-ilgili-algoritma-soruları/koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace koleksiyonlar_soru_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Soru - 3: Klavyeden girilen cümle içerisindeki sesli harfleri bir dizi içerisinde saklayan ve dizinin elemanlarını sıralayan programı yazınız.
+
+            Console.WriteLine("Bir Cümle Yazınız: ");
+            string cümle = Console.ReadLine();
+            string sesli = "aeıioöuü";
+            Console.Write("Cümledeki Sesli Harfler: ");
+            for (int i = 0; i < cümle.Length; i++)
+            {
+                if(sesli.Contains(cümle[i]))
+                {
+                    Console.Write(cümle[i]+" ");
+                }
+            }            
+        }
+    }
+}
